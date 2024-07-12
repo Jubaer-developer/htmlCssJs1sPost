96faabf4860151dd856bc91ea0df8e4d1397ab13
Jubaer-developer <jubaerhasan154@gmail.com>
2024-07-12 21:01:00 +0600

    The work is finish

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+/* dark theme code start */
+let themeIcon = document.getElementsByClassName("icon");
+
+if (localStorage.getItem("value")) {
+  document.body.classList.toggle(localStorage.getItem("value"));
+  themeIcon[0].src = localStorage.getItem("icon");
+}
+themeIcon[0].onclick = () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    themeIcon[0].src = "../../img/sun.png";
+  } else {
+    themeIcon[0].src = "../../img/moon.png";
+    localStorage.removeItem("value");
+    localStorage.removeItem("icon");
+  }
+  localStorage.setItem("value", document.body.classList.value);
+  localStorage.setItem("icon", themeIcon[0].src);
+  console.log(document.body.classList.value);
+};
+
+/* dark theme code end */
